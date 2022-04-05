8ce888f3c9d0e31d7dc193e7a2c2716cddf1b582
Sheharyar Naseer <hello@sheharyar.me>
2022-04-05 11:46:40 -0700

    Add sliders

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-window.addEventListener('load',function(){
+const canvas = document.querySelector('#canvas');
 
+const sliders = {
+    size: document.querySelector('#sliders #size'),
+    sides: document.querySelector('#sliders #sides'),
+}
 
-    const canvas = this.document.getElementById('canvas1');
+const defaultEffects = {
+    size: 200,
+    sides: 5,
+    maxLevel: 3,
+    scale: 0.5,
+    spread: 0.8,
+    branches: 2,
+}
+
+const renderFractal = ({ size, sides, maxLevel, scale, spread, branches }) => {
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -13,93 +26,61 @@ window.addEventListener('load',function(){
     ctx.lineWidth = 20;
     ctx.lineCap = 'round';
 
+    const drawBranch = (level) => {
+        if (level > maxLevel) return;
 
-    ///effect settings//
+        ctx.beginPath();
+        ctx.moveTo(0,0);
+        ctx.lineTo(size,0);
+        ctx.stroke();
 
-    let size = 200; 
-    let sides = 5;
-    let maxLevel =3;
-    let scale = 0.5;
-    let spread = 0.8;
-    let branches = 2;
-
-    ///something//
-  
-
-  //ctx.fillRect(0,0,canvas.width,canvas.height);
-    
-
-
-function drawBranch(level)
-{
-    if (level>maxLevel)return;
-    ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(size,0);
-    ctx.stroke();
-
-    for(let i = 0; i<branches;i++ )
-    
-    {
-        ctx.save();
-        ctx.translate(size-(size/branches)*i,0);
-        ctx.rotate(spread);
-        ctx.scale(scale,scale);
-        drawBranch(level +1);
-        ctx.restore();
-    
-    
-        ctx.save();
-        ctx.translate(size-(size/branches)*i,0);
-        ctx.rotate(-spread);
-        ctx.scale(scale,scale);
-        drawBranch(level +1);
-        ctx.restore();
-
+        for (let i = 0; i < branches; i++) {
+            ctx.save();
+            ctx.translate(size-(size/branches)*i,0);
+            ctx.rotate(spread);
+            ctx.scale(scale,scale);
+            drawBranch(level +1);
+            ctx.restore();
+        
+            ctx.save();
+            ctx.translate(size-(size/branches)*i,0);
+            ctx.rotate(-spread);
+            ctx.scale(scale,scale);
+            drawBranch(level +1);
+            ctx.restore();
+        }
     }
-}
 
-
-
-
-function drawFractal(){
     ctx.save();
     ctx.translate(canvas.width/2,canvas.height/2);
     ctx.scale(scale,scale);
     ctx.rotate(0);
 
-    for(let i=0; i<sides; i++ ){
-    ctx.rotate((Math.PI * 2)/sides);
-    drawBranch(0);
-        }
-        ctx.restore();
-}
-
-drawFractal();
-
-
-    //something///
-
-    /*
-    for(let i=0; i<sides; i++ ){
-
-    ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(size,0);
-    ctx.stroke();
-    ctx.rotate((Math.PI * 2)/sides);
+    for (let i = 0; i < sides; i++) {
+        ctx.rotate((Math.PI * 2)/sides);
+        drawBranch(0);
     }
 
-*/
-  
+    ctx.restore();
+};
 
+const getValue = (input) => parseInt(input.value);
 
-
-
+const renderFractalFromInput = () => {
+    const size = getValue(sliders.size);
+    const sides = getValue(sliders.sides);
     
+    renderFractal({...defaultEffects, size, sides})
+}
+
+
+window.addEventListener('load',function(){
+    renderFractal(defaultEffects)
 })
 
 
+sliders.size.oninput = renderFractalFromInput
+sliders.sides.oninput = renderFractalFromInput
 
 
 
